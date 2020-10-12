a329b8d5708d2c6d1a2a141ab1b74d82dd9cf846
MBerry97 <imatty2011@hotmail.co.uk>
2020-10-12 16:23:12 +0100

    Added name and year onto marker positions

--- src/components/HeatMap.jsx
+++ src/components/HeatMap.jsx
@@ -5,26 +5,32 @@ import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 const HeatMap = (props) => {
   const { meteorites } = props.meteoriteInfo;
   const { startingYear, endingYear, mass } = props.meteoriteInfo.userInputs;
-  const centerPosition = [51.505, -0.09];
+  const centerPosition = [0, 0];
   return (
     <div className='leaflet-container'>
-      <Map center={centerPosition} zoom={3}>
+      <Map center={centerPosition} zoom={2}>
         <TileLayer
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
         {meteorites.map((meteorite) => {
-          const { geolocation, name } = meteorite;
+          const { geolocation, name, year } = meteorite;
+          console.log(year);
 
-          console.log(geolocation);
           if (geolocation) {
             const { latitude, longitude } = geolocation;
             const position = [latitude, longitude];
-            return (
-              <Marker position={position}>
-                <Popup>{name}</Popup>
-              </Marker>
-            );
+            if (year) {
+              const onlyYear = year.substring(0, 4);
+              return (
+                <Marker position={position}>
+                  <Popup>
+                    {name} <br />
+                    {onlyYear}
+                  </Popup>
+                </Marker>
+              );
+            }
           }
         })}
       </Map>
